e312b063a204c1d398731319e39504b6f784fc08
abdelhafidsahli <abdelhafidsahli207@gmail.com>
2023-01-20 00:10:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import Cll from '..//src/Comporant/Com01'
-import Cal from './Comporant/Cal';
-import { useState } from 'react';
-import Lista from '../src/Comporant/List';
 import './App.css'
-import Radio from './Comporant/Radio';
-import UesRef from './Comporant/UesRef';
+
 
 
 function App() {
